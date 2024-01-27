505d2c0d8fb2d59aa99ad508ed46678a88827635
Ethan Hosier <ethanjhosier@gmail.com>
2024-01-27 20:02:53 +0000

    removed broken import 2

--- app/_components/templates/Hero.tsx
+++ app/_components/templates/Hero.tsx
@@ -8,7 +8,6 @@ import { Logo } from "./Logo";
 import { Button } from "@/components/ui/button";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
-import ChatBot from "@/app/chat/_components/chatbot";
 import Header from "@/app/(product)/chat/_components/header";
 const headingFont = localFont({
   src: "../../../public/fonts/font.woff2",
